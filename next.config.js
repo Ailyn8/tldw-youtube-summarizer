49d642bfc365e53165e894f1bcbac38330da1b9e
Ailyn8 <thespookyhalloween@gmail.com>
2025-12-30 14:44:15 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    serverComponentsExternalPackages: ['youtube-transcript'],
+    serverComponentsExternalPackages: ['youtube-captions-scraper'],
   },
 };
 
